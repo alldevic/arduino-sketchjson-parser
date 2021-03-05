4370ef093c9c4596553734259af7e327b421c2b1
Nikolay Bely <belyynikolay@gmail.com>
2021-03-05 13:33:29 +0700

    Update utils.test.ts

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -26,5 +26,5 @@ test('LibList', async () => {
         { name: "test2", version: "" },
         { name: "test3", version: "v1.2.3" },
     ];
-    expect(getLibs(stub)).toBe("test1,test1@1,test2,test3@v1.2.3");
+    expect(getLibs(stub)).toBe("test,test1@1,test2,test3@v1.2.3");
 });
